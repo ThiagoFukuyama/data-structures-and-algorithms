673b3eefecda0f1037ac7cc0e11a0c27031c527e
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2023-12-28 18:15:58 -0300

    Atualiza texto no index principal

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 console.log("Algoritmos e Estruturas de Dados (TypeScript)");
-console.log('Os módulos podem ser importados de "./dataStructures/index"');
+console.log(
+    'Os módulos podem ser importados de "./dataStructures/index" e "./algorithms/index"'
+);
