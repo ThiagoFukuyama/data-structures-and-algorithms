96d59900cfc7044eeec68c8426aba26e75bd0616
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2024-03-23 21:09:39 -0300

    Remove possível falso positivo do selectionSort
    (Não sei porque fiz aquilo)

--- src/algorithms/selectionSort/selectionSort.ts
+++ src/algorithms/selectionSort/selectionSort.ts
@@ -1,18 +1,11 @@
 export function selectionSort(array: number[]): number[] {
-    let newMinFound = false;
-
     for (let i = 0; i < array.length - 1; i++) {
         let min = i;
 
         for (let j = i + 1; j < array.length; j++) {
             if (array[j] < array[min]) {
                 min = j;
-                newMinFound = true;
             }
-        }
-
-        if (!newMinFound) {
-            break;
         }
 
         [array[i], array[min]] = [array[min], array[i]];
